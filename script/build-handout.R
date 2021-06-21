2024f635bd5e9e3def9988a7243277131c3f9b62
Tamara Cook <tamara.cook@uni-marburg.de>
2021-06-21 17:49:43 +0200

    Build: Setup npm package and husky

---
+++ script/build-handout.R
@@ -0,0 +1,2 @@
+#!/usr/bin/env Rscript
+rmarkdown::render("source/handout.Rmd")
---
+++ script/build-slides.R
@@ -0,0 +1,2 @@
+#!/usr/bin/env Rscript
+rmarkdown::render("source/presentation.Rmd", output_dir = "build", output_file = "index.html")
